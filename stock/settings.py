d271d6782c72571778338b255dbdf401452f1b53
mage1028 <524538131@qq.com>
2018-06-07 20:48:17 +0800

    delay

--- stock/settings.py
+++ stock/settings.py
@@ -26,7 +26,10 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5
+LOG_ENABLED = True
+DOWNLOAD_DELAY = 0.2
+RANDOMIZE_DOWNLOAD_DELAY = True
+LOG_FILE = "/hbasestorage/spider/spider_logs/stock_type/scrapy.log"
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
